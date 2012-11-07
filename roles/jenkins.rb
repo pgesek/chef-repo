8333f7527fb19136157ffb5c4a0b6355b3fe80fa
Paweł Gesek <pgesek@soldevelo.com>
2012-11-07 18:08:02 +0100

    Added jenkins role

---
+++ roles/jenkins.rb
@@ -0,0 +1,31 @@
+name "jenkins"
+description "Jenkins CI server"
+
+run_list "role[oracle-jdk7]",
+         "recipe[apt]",
+         "recipe[maven::maven3]",
+         "recipe[ant]",
+         "recipe[git]",
+         "recipe[mysql::server]",
+         "recipe[mysql::client]",
+         "recipe[sqlite]",
+         "recipe[couchdb]",
+         "recipe[activemq]",
+         "recipe[perl]",
+         "recipe[jenkins]"
+
+override_attributes(
+  :jenkins => {
+    :server => { 
+      :plugins => ["git", "greenballs", "locks-and-latches", "sonar", "performance", "github", "ci-game"],
+      :host => "localhost",
+      :port => 8060
+    }
+  },
+  :maven => {
+    :version => 3,
+    "3" => {
+        :url => "http://www.apache.org/dist/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz"
+    }
+  }
+)
